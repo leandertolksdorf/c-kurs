29d566f0fb0c4b2b97f60a0950b4821659981fa5
Leander Tolksdorf <leandet98@andorra.imp.fu-berlin.de>
2020-05-07 19:12:32 +0200

    hallo

--- Uebung_1/cat.c
+++ Uebung_1/cat.c
@@ -4,8 +4,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFFERSIZE 16 // Define size of buffer.
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
 
 int main(int argc, char const *argv[]) {
 
@@ -13,25 +17,39 @@ int main(int argc, char const *argv[]) {
     int fd, i;
     const int size = argc;
     const char nl = '\n';
+    const char* file_error = "File not found.\n";
     ssize_t readBytes;
 
     // Allocate memory for buffer.
     char *buf = malloc(BUFFERSIZE);
+    // If filenames given, read files. Else read from STDIN.
 
-    // Iterate over Arguments (files).
-    for(i=1; i < size; i++) {
-        // Save file descriptor of i-th file to fd.
-        fd = open(argv[i], O_RDONLY);
-        
+    if(argc != 1) {
+        // Iterate over Arguments (files).
+        for(i=1; i < size; i++) {
+            // Save file descriptor of i-th file to fd.
+            fd = open(argv[i], O_RDONLY);
+            // If file doesn't exist:  return error.
+            if (fd == -1) {
+                write(STDERR, file_error, 16);
+                return -1;
+            }
+            
+            do {
+                // Read from fd to buffer. Save number of read bytes.
+                readBytes = read(fd, buf, BUFFERSIZE);
+                // Write read bytes to file-descriptor 1 (stdout).
+                write(STDOUT, buf, readBytes);
+                // Repeat until end of file.
+            } while (readBytes != 0);
+            // New line after file.
+            write(STDOUT, &nl, 1);
+        }
+    } else {
         do {
-            // Read from fd to buffer. Save number of read bytes.
-            readBytes = read(fd, buf, BUFFERSIZE);
-            // Write read bytes to file-descriptor 1 (stdout).
-            write(1, buf, readBytes);
-            // Repeat until end of file.
-        } while(readBytes == BUFFERSIZE);
-        // New line after file.
-        write(1, &nl, 1);
+            readBytes = read(STDIN, buf, BUFFERSIZE);
+            write(STDOUT, buf, readBytes);
+        } while (readBytes != 0);
     }
 
     return 0;
