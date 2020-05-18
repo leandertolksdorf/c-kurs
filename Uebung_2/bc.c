01f31106db1c7eddc1f627a2883dbbd3f858b435
Simon <49556357+franke94@users.noreply.github.com>
2020-05-18 19:56:14 +0200

    Update bc.c

--- Uebung_2/bc.c
+++ Uebung_2/bc.c
@@ -16,34 +16,53 @@ int calc(int op1, char op, int op2) {
 }
 
 void readStdin() {
-    int op1, op2, result;
+
+    int op1, op2, result, test;
     char op;
     while(1) {
-        scanf("%d %c %d", &op1, &op, &op2);
-        result = calc(op1, op, op2);
-        printf("%d", result);
+        
+	test = scanf("%d %c %d", &op1, &op, &op2);
+	if(test != 3){
+	    printf("Fehler, falsche Eingabe\n");
+	    break;
+	}else{
+	    
+            result = calc(op1, op, op2);
+            printf("%d\n", result);
+	}
     }
 }
 
 int main(int argc, char const *argv[]) {
 
     FILE *fs;
-    int op1, op2, result;
+    int op1, op2, result, test;
     char op;
+    
 
     if(argc != 1) {
         for(int i=1; i < argc; i++) {
             // Get file stream pointer
             fs = fopen(argv[i], "r");
-            while(fscanf(fs, "%d %c %d", &op1, &op, &op2) != EOF) {
-                printf("%d %c %d\n", op1, op, op2);
-                result = calc(op1, op, op2);
-                printf("%d\n", result);
+		
+            while((test = fscanf(fs, "%d %c %d", &op1, &op, &op2)) != EOF) {
+		if(test != 3){
+		    printf("Fehler, falsche Werte in Datei\n");
+	    	    break; 
+		}else{
+                    printf("%d %c %d\n", op1, op, op2);
+                    result = calc(op1, op, op2);
+                    printf("%d\n", result);
+
+		    }
+		
             }
         }
+	
     }
+    else{
 
-    readStdin();
-
+        readStdin();
+    }
     return 0;
 }
