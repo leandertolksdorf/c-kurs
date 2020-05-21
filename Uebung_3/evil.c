1a2b2a0ac5bd4ca5dc791f99569ef04341159724
Leander Tolksdorf <leandet98@andorra.imp.fu-berlin.de>
2020-05-21 15:06:22 +0200

    Übung 3, Aufgabe 2

--- Uebung_3/evil.c
+++ Uebung_3/evil.c
@@ -1,25 +1,36 @@
 #include <stdio.h> 
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
 
+#define _OPEN_SYS_ITOA_EXT
 #define _POSIX_C_SOURCE 199309L
+
+/*
+To do
+- Print PID using write at the beginning?
+- Convert handler prints to write calls.
+*/
 
 // Define handlers for SIGINT and SIGTERM
 
 void sigIntHandler(int sigNum) {
+    (void) sigNum;
     printf("sigIntHandler: Böse Nachricht\n");
 }
 
 void sigTermHandler(int sigNum) {
+    (void) sigNum;
     printf("sigTermHandler: Böse Nachricht\n");
 }
 
 int main(int argc, char const *argv[])
 {
-    int pid = getpid();
+    const void *pid = getpid();
 
+    printf("%d", pid);
     // Define structs for SIGINT- and SIGTERM-handling.
     struct sigaction sigInt;
     struct sigaction sigTerm;
@@ -31,6 +42,7 @@ int main(int argc, char const *argv[])
 
     // Set sleep time to 2 seconds.
     sleep.tv_sec = 2;
+    sleep.tv_nsec = 0;
 
     // Declare procedure on SIGINT and SIGTERM-signals.
     sigaction(SIGINT, &sigInt, NULL);
@@ -38,8 +50,9 @@ int main(int argc, char const *argv[])
 
     // Print PID continually.
     while(1){
-        printf("Endlosschleife | PID: %d\n", pid);
-        nanosleep(&sleep, NULL);
+        write(STDOUT_FILENO, pid, sizeof(pid));
+        write(STDOUT_FILENO, "Endlosschleife\n", 15);
+        nanosleep(&sleep, (struct timespec *)NULL);
     }
 
     return 0;
--- Uebung_3/kill.c
+++ Uebung_3/kill.c
@@ -1,4 +1,34 @@
-int main(int argc, char const *argv[])
-{
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#define _POSIX_C_SOURCE 199309L
+/*
+To do
+- Handling cmd line option as signal number if given. (-> getops()?)
+*/
+
+
+int main(int argc, char const *argv[]) {
+    //int sigNum = 15; // Default signal if no options given.
+    int first = 1; // First PID (either 1 or if sigNum gíven -> 2)
+    int sigNum = 15;
+    pid_t pid;
+
+    int firstArg = atoi(argv[1]);
+
+    if(firstArg < 0) {
+        sigNum = firstArg * (-1);
+        first = 2;
+    }
+
+    //printf("%d", sigNum);
+    // Iterate through PIDs.
+    for(int i = first; i < argc; i++) {
+        pid = atoi(argv[i]);
+        kill(pid, sigNum);
+    }
+
+
     return 0;
 }
