4e5a55d8ea4dccf9207b492660348d29350db966
Felix <11788526+flxsu@users.noreply.github.com>
2020-09-14 23:42:43 +0200

    kleine Fixes

--- ProInf/Uebung_6/bubblesort.asm
+++ ProInf/Uebung_6/bubblesort.asm
@@ -3,51 +3,55 @@ GLOBAL sort
 section .data
     %define LENGTH rdi
     %define ARRAY rsi
+    %define I r8
+    %define J r9
 
 section .text
 
 sort:
     ; i = array.length
-    mov r8, LENGTH
+    mov I, LENGTH
     .outer_loop:
-        ; loop condition: i > 1
-        cmp r8, 1
+        ; continue loop if: i > 1
+        cmp I, 1
         jna .outer_end
         ; j = 0
-        mov r9, 0
+        mov J, 0
         .inner_loop:
-            ; loop condition: j < i-1
-            mov rcx, r8
+            ; rcx = i-1
+            mov rcx, I
             dec rcx
-            cmp r9, rcx
+            ; continue loop if: j < i-1
+            cmp J, rcx
             jnb .inner_end
-            .debug:
             .if_condition:
-                ; j+1
-                mov rcx, r9
+                ; rcx = j+1
+                mov rcx, J
                 inc rcx
-                ; extract array[j] and array[j+1]
-                mov rax, qword [ARRAY + 8*r9]
+                ; rax = array[j]
+                ; rdx = array[j+1]
+                mov rax, qword [ARRAY + 8*J]
                 mov rdx, qword [ARRAY + 8*(rcx)]
-                ; condition: array[j] > array[j+1]
+                ; execute if: array[j] > array[j+1]
                 cmp rax, rdx
                 jna .inner_continue
-                ; swap array[j] and array[j+1]
-                ; array[j]
-                mov r10, qword [ARRAY + 8*r9]
-                ; array[j+1]
+                ; swap array[j] with array[j+1]
+                ; r10 = array[j]
+                mov r10, qword [ARRAY + 8*J]
+                ; r11 = array[j+1]
                 mov r11, qword [ARRAY + 8*(rcx)]
-                ; re-assign
-                mov qword [ARRAY + 8*r9], r11
+                ; array[j] = r11
+                ; array[j+1] = r10
+                mov qword [ARRAY + 8*J], r11
                 mov qword [ARRAY + 8*(rcx)], r10
         .inner_continue:
             ; j++
-            inc r9
+            inc J
             jmp .inner_loop
         .inner_end:
     .outer_continue:
         ; i--
-        dec r8
+        dec I
         jmp .outer_loop
     .outer_end:
 
